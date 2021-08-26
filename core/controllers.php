e02b0185d082b231e1fa931cd933bf245130c011
Boaz <webevolution13@gmail.com>
2021-08-26 10:20:47 +0200

    Back - end (own code)

---
+++ core/controllers.php
@@ -0,0 +1,24 @@
+<?php
+
+function homeController()
+{
+
+
+    return [
+        "home",
+        [
+            "title" => "Kezdőlap"
+        ]
+    ];
+}
+
+function notFoundController()
+{
+    return [
+        "404", [
+            "title" => "A keresett oldal nem található."
+        ]
+    ];
+}
+
+
