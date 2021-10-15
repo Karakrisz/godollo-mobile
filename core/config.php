e3b23937587e5694d74e85e6365410bc016c2a22
Boaz <webevolution13@gmail.com>
2021-10-15 15:08:58 +0200

    Backup

--- core/config.php
+++ core/config.php
@@ -7,6 +7,13 @@ $config = [
     "DB_NAME" => "godollo_mobil"
 ];
 
+// $config = [
+//     "DB_HOST" => "localhost",
+//     "DB_USER" => "gsmszerv_karaKrisz",
+//     "DB_PASS" => "Hacker13prog",
+//     "DB_NAME" => "gsmszerv_godollo_mobil"
+// ];
+
 $connection = mysqli_connect($config['DB_HOST'], $config['DB_USER'], $config['DB_PASS'], $config['DB_NAME']);
 $sql = "set names utf8";
 mysqli_query($connection, $sql);
